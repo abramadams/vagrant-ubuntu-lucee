be4647634c0d69acaa1d48291bf806c8a8079051
abramadams <aadams@cfxchange.com>
2015-11-03 11:40:20 -0800

    Update step-4-install-lucee.sh
    Updated lucee version

--- bash-scripts/step-4-install-lucee.sh
+++ bash-scripts/step-4-install-lucee.sh
@@ -9,22 +9,22 @@ if [ ! -d "/opt/lucee" ]; then
 
 	
 	# Don't download if we've already got it locally
-	if [ -f "/vagrant/artifacts/lucee-4.5.1.000-pl0-linux-x64-installer.run" ]; then
+	if [ -f "/vagrant/artifacts/lucee-4.5.1.022-pl1-linux-x64-installer.run" ]; then
 		echo "... Copying Lucee installer ..."
-		sudo cp /vagrant/artifacts/lucee-4.5.1.000-pl0-linux-x64-installer.run /root
+		sudo cp /vagrant/artifacts/lucee-4.5.1.022-pl1-linux-x64-installer.run /root
 	else
 		echo "... Downloading the Lucee installer, standby ..."
-		wget -O /root/lucee-4.5.1.000-pl0-linux-x64-installer.run http://railo.viviotech.net/downloader.cfm/id/133/file/lucee-4.5.1.000-pl0-linux-x64-installer.run &> /dev/null
-		sudo cp /root/lucee-4.5.1.000-pl0-linux-x64-installer.run /vagrant/artifacts
+		wget -O /root/lucee-4.5.1.022-pl1-linux-x64-installer.run http://railo.viviotech.net/downloader.cfm/id/133/file/lucee-4.5.1.022-pl1-linux-x64-installer.run &> /dev/null
+		sudo cp /root/lucee-4.5.1.022-pl1-linux-x64-installer.run /vagrant/artifacts
 
 	fi
 	
 	
 	
 	echo "... Installing Lucee ..."
-	chmod +x /root/lucee-4.5.1.000-pl0-linux-x64-installer.run
+	chmod +x /root/lucee-4.5.1.022-pl1-linux-x64-installer.run
 	cp /vagrant/configs/lucee-options.txt /root
-	/root/lucee-4.5.1.000-pl0-linux-x64-installer.run --mode unattended --optionfile /root/lucee-options.txt
+	/root/lucee-4.5.1.022-pl1-linux-x64-installer.run --mode unattended --optionfile /root/lucee-options.txt
 fi
 
 
